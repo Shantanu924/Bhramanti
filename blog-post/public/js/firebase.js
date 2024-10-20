b4117c35ed868397bc679e9f6b79d2c032947f30
Sarang <shantanubhukan2409@gmail.com>
2024-10-20 13:35:01 +0530

    video back

--- blog-post/public/js/firebase.js
+++ blog-post/public/js/firebase.js
@@ -1,10 +1,19 @@
-let firebaseConfig = {
-    PROJECT_NAME: 'BHRAMANTI',
-   PROJECT_ID: 'bhramanti-2024',
-   PROJECT_NUMBER: 671555597189,
-   DATABASE:'BHRAMANTI'
+import {initializeApp} from "firebase/app";
+import {getAnalytics} from "firebase/analytics";
+import {getFirestore} from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBHCd0MGZoGCOf60WeomPZUMADs5uEGLOU",
+    authDomain: "bharamanti.firebaseapp.com",
+    projectId: "bharamanti",
+    storageBucket: "bharamanti.appspot.com",
+    messagingSenderId: "124592706261",
+    appId:"1:124592706261:web:6b41e4fa89f878eeed885c",
+    measurementId: "G-QXZR37J8E0"
 };
 
-firebase.initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
+const analytics = firebase.getAnalytics(firebaseConfig);
+let db = firebase.getFirestore();
 
-let db = firebase.firestore();
+module.exports={app,analytics,db}
